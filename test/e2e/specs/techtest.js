92d90a087544ef9a7a9c30f4c2ab0612bd381683
Ted Newton <edwardfrancisnewton@live.com>
2018-02-28 22:03:33 +0000

    added a test to remove an item, it fails so will fix the code to make it pass

--- test/e2e/specs/techtest.js
+++ test/e2e/specs/techtest.js
@@ -6,10 +6,11 @@ module.exports = {
       .assert.visible('#app')
 
     // tests search form works for the camera
-    .setValue('.form-control', 'camera')
+    .setValue('.form-control', 'Camera')
       browser.expect.element('#app').text.to.not.contain('USB Stick');
       browser.expect.element('#app').text.to.not.contain('USB Plug');
       browser.expect.element('#app').text.to.not.contain('Small Phone');
+      browser.expect.element('#app').text.to.contain('Camera');
 
     // tests search form works for the USB Plug
     browser
@@ -18,6 +19,7 @@ module.exports = {
       browser.expect.element('#app').text.to.not.contain('USB Stick');
       browser.expect.element('#app').text.to.not.contain('Camera');
       browser.expect.element('#app').text.to.not.contain('Small Phone');
+      browser.expect.element('#app').text.to.contain('USB Plug');
 
     // tests search form works for the USB Stick
     browser
@@ -26,6 +28,7 @@ module.exports = {
       browser.expect.element('#app').text.to.not.contain('USB Plug');
       browser.expect.element('#app').text.to.not.contain('Camera');
       browser.expect.element('#app').text.to.not.contain('Small Phone');
+      browser.expect.element('#app').text.to.contain('USB Stick');
 
     // tests search form works for the phone
     browser
@@ -34,6 +37,7 @@ module.exports = {
       browser.expect.element('#app').text.to.not.contain('USB Plug');
       browser.expect.element('#app').text.to.not.contain('Camera');
       browser.expect.element('#app').text.to.not.contain('USB Stick');
+      browser.expect.element('#app').text.to.contain('Small Phone');
 
     // tests the add to cart button works
     browser
@@ -55,5 +59,9 @@ module.exports = {
     .click('select[id=donation]')
     .click('option[value="10"]')
     .assert.containsText('.list-unstyled', '$189.10')
+
+    browser
+    .click('.btn btn-xs btn-danger btn-delete')
+    .assert.containsText('.list-unstyled', '0 Items')
   }
 }
