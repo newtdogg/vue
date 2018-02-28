c0b0d52ea2cc2e59ee2c8cbdbc2fd3baafacea85
Ted Newton <edwardfrancisnewton@live.com>
2018-02-28 19:43:40 +0000

    added further search bar tests for the other items that can be searched for, all tests pass

--- test/e2e/specs/techtest.js
+++ test/e2e/specs/techtest.js
@@ -9,5 +9,26 @@ module.exports = {
       browser.expect.element('#app').text.to.not.contain('USB Stick');
       browser.expect.element('#app').text.to.not.contain('USB Plug');
       browser.expect.element('#app').text.to.not.contain('Small Phone');
+
+    browser
+    .clearValue('.form-control')
+    .setValue('.form-control', 'USB Plug')
+      browser.expect.element('#app').text.to.not.contain('USB Stick');
+      browser.expect.element('#app').text.to.not.contain('Camera');
+      browser.expect.element('#app').text.to.not.contain('Small Phone');
+
+    browser
+    .clearValue('.form-control')
+    .setValue('.form-control', 'USB Stick')
+      browser.expect.element('#app').text.to.not.contain('USB Plug');
+      browser.expect.element('#app').text.to.not.contain('Camera');
+      browser.expect.element('#app').text.to.not.contain('Small Phone');
+
+    browser
+    .clearValue('.form-control')
+    .setValue('.form-control', 'Small Phone')
+      browser.expect.element('#app').text.to.not.contain('USB Plug');
+      browser.expect.element('#app').text.to.not.contain('Camera');
+      browser.expect.element('#app').text.to.not.contain('USB Stick');
   }
 }
